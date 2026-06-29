86a06fa5980fd7f3bb92f0306598a1dc1bf3c285
estban03 <estebanmutuverria003@gmail.com>
2026-06-28 23:18:23 -0300

    changes in cartConfig

--- src/constants/cartConfig.js
+++ src/constants/cartConfig.js
@@ -5,5 +5,5 @@
 
 export const CART_CONFIG = {
     // Número de teléfono de destino para pedidos en WhatsApp (sin el "+" o caracteres especiales)
-    whatsappNumber: '1160081534',
+    whatsappNumber: '5491160081534',
 };
